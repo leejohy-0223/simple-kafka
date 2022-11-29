077551b5f4c3baaa8fb202d20d82607a51bfcf2c
leejohy-0223 <leejohy@naver.com>
2022-11-29 13:10:07 +0900

    feat: using simple ProducerRecord

--- src/main/java/com/example/SimpleProducer.java
+++ src/main/java/com/example/SimpleProducer.java
@@ -1,4 +1,34 @@
 package com.example;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SimpleProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleProducer.class);
+    private static final String TOPIC_NAME = "test";
+    private static final String BOOTSTRAP_SERVER = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        String messageValue = "testMessage3";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "23");
+        producer.send(record);
+        log.info("record info is {}", record);
+        producer.flush();
+        producer.close();
+    }
 }
